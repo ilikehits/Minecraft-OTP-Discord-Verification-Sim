22157ecde698b233900bb825c569090f5bca5cc1
ilikehits <michal.wisniewski624@wp.pl>
2026-08-27 20:40:39 +0200

    Load environment variables for TOKEN and MAILSLURP_API_KEY

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
-TOKEN = ""
+import os
+
+TOKEN = os.getenv("TOKEN")
 API_KEY = ""
-MAILSLURP_API_KEY=""
+MAILSLURP_API_KEY = os.getenv("MAILSLURP_API_KEY")
 
-
-#Dont Fill These In
-LastUsedEmail=""
-LastUserName=""
-LastRecoveryCode=""
-AUTHVALUE=""
+# Dont Fill These In
+LastUsedEmail = ""
+LastUserName = ""
+LastRecoveryCode = ""
+AUTHVALUE = ""

3ca8e34923d8d045d386fddff684eadebeedf100
ilikehits <michal.wisniewski624@wp.pl>
2026-08-27 22:10:12 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,6 @@ TOKEN = os.getenv("TOKEN")
 API_KEY = ""
 MAILSLURP_API_KEY = os.getenv("MAILSLURP_API_KEY")
 
-print("TOKEN ustawiony:", bool(TOKEN))
-print("TOKEN długość:", len(TOKEN or ""))
-
 LastUsedEmail = ""
 LastUserName = ""
 LastRecoveryCode = ""
